aa6da0f8bcad1745073e7325f3fb90785607c0dd
Philip Yi <philip.yi@duke.edu>
2023-10-05 22:04:13 -0400

    finished for milestone 3

---
+++ pages/header.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { AspectRatio, Box, Flex, Image, Button, Text, Table, TableContainer, Tbody, Tr, Td } from '@chakra-ui/react'
+
+const header = () => {
+  return (
+    <div>header</div>
+  )
+}
+
+export default header
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,6 +1,8 @@
 //display existing profile
 import * as React from 'react'
-import { AspectRatio, Box, Flex, Spacer, Image, Heading, Button } from '@chakra-ui/react'
+import { AspectRatio, Box, Flex, Image, Divider, Text, Card, CardHeader, CardBody, Icon} from '@chakra-ui/react'
+import { MdSettings } from 'react-icons/md'
+import { AiOutlineUser } from 'react-icons/ai';
 import { useSession } from "next-auth/react"
 import { authOptions } from "./api/auth/[...nextauth]"
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
@@ -14,13 +16,78 @@ import { useRouter } from 'next/navigation';
 //user, email, sexual pref, 10 animes you like, genre, profile picture, gender, Recommended Anime
 const Page = ( { profile }: {profile: ProfileProps}) => {
     console.log(profile);
-    return (
-        <Flex flexDirection={"column"}>
-            <Box>
-                <h1>{profile.username}</h1>
-            </Box>
+    const img_size = 125;
+    var gender = "Female"
+    if(profile.gender == 'M')
+    {
+        gender = "Male"
+    }
+    var pref = "He/Him"
 
-            <h1>BOOOHOOOO</h1>
+    return (
+        <Flex flexDirection="column">
+            <Flex flexDirection="column" justifyContent={'flex-end'} bg='#530303' height={'200px'} color={'white'}>
+                <Flex flexDirection={'row'} justifyContent={"space-between"} alignItems={'flex-end'} gap={50}>
+                    {/**profile picture will be obtained through s3 bucket */}
+                    <Flex flexDirection={'row'} alignItems={'flex-end'}>
+                        <Image 
+                        src={"/magflake.jpeg"} 
+                        alt='profile picture' 
+                        width={150} height={150} 
+                        marginRight={2} marginLeft={200} marginBottom={6}/> 
+                        <Text marginBottom={6} fontSize={25}>{profile.username}</Text>
+                    </Flex>
+                    <Flex>
+                        <Card bg='white' width={'400px'} marginBottom={6} marginRight={100}>
+                                <CardBody>
+                                    <Flex flexDirection={'row'} justifyContent={'flex-start'} alignContent={'flex-end'}>
+                                        <AiOutlineUser as={MdSettings}/>
+                                        <Text colorScheme='blackAlpha' paddingLeft={1}>{gender}</Text>
+                                    </Flex>
+                                </CardBody>
+
+                            <CardBody>
+                                <Text><b>Bio</b></Text>
+                                <Divider/>
+                                <Text>{profile.bio}</Text>
+                            </CardBody>
+                        </Card>
+                    </Flex>
+                </Flex>
+                <Flex>
+
+                </Flex>
+            </Flex>
+            <Flex flexDirection={"column"} justifyContent={'flex-start'} alignItems={'center'}>
+                <Text width={'500px'} color={'gray.400'} fontSize={15}><b>My Photos</b></Text>
+                <Flex flexDirection={"row"} justifyContent={'center'} alignContent={'center'}>
+                    <Image 
+                        src={"horimiya.png"} 
+                        alt='horimiya' 
+                        width={img_size} height={img_size}
+                        margin={"5px"} 
+                    />
+                    <Image 
+                        src={"cowboybebop.png"} 
+                        alt='cowboybebop' 
+                        width={img_size} height={img_size} 
+                        margin={"5px"} 
+                    />
+                    <Image 
+                        src={"aot.jpeg"} 
+                        alt='aot' 
+                        width={img_size} height={img_size} 
+                        margin={"5px"} 
+                    />
+                    <Image 
+                        src={"demonslayer.jpeg"} 
+                        alt='demonslayer' 
+                        width={img_size} height={img_size} 
+                        margin={"5px"} 
+                    />
+                </Flex>
+            </Flex>
+
         </Flex>
       )
 }
@@ -74,14 +141,11 @@ export const getServerSideProps = (async (context) => {
 
 const profile = ({response} : InferGetServerSidePropsType<typeof getServerSideProps>) => {
     const profile_data : ProfileProps = response;
-
     return (
                 <Page profile={profile_data} />
         );
 
 }
-
-
 
 export default profile
 
